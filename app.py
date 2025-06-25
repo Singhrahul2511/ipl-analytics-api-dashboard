03e01d5365559370eae39ac2ef4fb87c7f267cb1
RAHUL KUMAR <147340802+Singhrahul2511@users.noreply.github.com>
2025-06-26 03:09:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,8 @@ def player_stats():
     return render_template('index.html', result={}, teams=sorted(teams), player_stats=stats, player_name=corrected)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
